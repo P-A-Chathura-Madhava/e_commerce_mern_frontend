bf6c8559174144ab89335690f26d1233e215bc44
P A Chathura Madhava <chathuratechdev@gmail.com>
2023-08-13 09:19:23 +0530

    apply footer

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,20 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+    <section className='home-wrapper-1 py-5'>
+      <div className='container-xxl'>
+        <div className='row'>
+          <div className='col-6'>
+            <div className='main-banner-content p-3'>
+              <img src='images/main-banner-1.jpg' className='img-fluid rounded-3' alt='main banner'/>
+            </div>
+          </div>
+          <div className='col-6'></div>
+        </div>
+      </div>
+    </section>
+    </>
   )
 }
 
