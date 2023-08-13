80e568a0455efb99f19f2ac28c9406ff38a24b67
P A Chathura Madhava <chathuratechdev@gmail.com>
2023-08-13 11:05:48 +0530

    apply section one in Home Page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,93 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <>
-    <section className='home-wrapper-1 py-5'>
-      <div className='container-xxl'>
-        <div className='row'>
-          <div className='col-6'>
-            <div className='main-banner-content p-3'>
-              <img src='images/main-banner-1.jpg' className='img-fluid rounded-3' alt='main banner'/>
+      <section className="home-wrapper-1 py-5">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-6">
+              <div className="main-banner position-relative">
+                <img
+                  src="images/main-banner-1.jpg"
+                  className="img-fluid rounded-3"
+                  alt="main banner"
+                />
+                <div className="main-banner-content position-absolute">
+                  <h4>SUPERCHARGED FOR PROS.</h4>
+                  <h5>iPad s13+ Pro.</h5>
+                  <p>From 160000LKR or 200000LKR</p>
+                  <Link className="button">BUY NOW</Link>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-6">
+              <div className="d-flex flex-wrap gap-10 justify-content-between align-items-center">
+                <div className="small-banner position-relative">
+                  <img
+                    src="images/catbanner-01.jpg"
+                    className="img-fluid rounded-3"
+                    alt="main banner"
+                  />
+                  <div className="small-banner-content position-absolute">
+                    <h4>Best Sale</h4>
+                    <h5>iPad s13+ Pro.</h5>
+                    <p>
+                      From 160000LKR <br /> or 200000LKR
+                    </p>
+                  </div>
+                </div>
+                <div className="small-banner position-relative">
+                  <img
+                    src="images/catbanner-02.jpg"
+                    className="img-fluid rounded-3"
+                    alt="main banner"
+                  />
+                  <div className="small-banner-content position-absolute">
+                    <h4>NEW ARRIVAL</h4>
+                    <h5>Buy IPad Air</h5>
+                    <p>
+                      From 160000LKR <br /> or 200000LKR
+                    </p>
+                  </div>
+                </div>
+                <div className="small-banner position-relative">
+                  <img
+                    src="images/catbanner-03.jpg"
+                    className="img-fluid rounded-3"
+                    alt="main banner"
+                  />
+                  <div className="small-banner-content position-absolute">
+                    <h4>SUPERCHARGED FOR PROS.</h4>
+                    <h5>iPad s13+ Pro.</h5>
+                    <p>
+                      From 160000LKR <br /> or 200000LKR
+                    </p>
+                  </div>
+                </div>
+                <div className="small-banner position-relative">
+                  <img
+                    src="images/catbanner-04.jpg"
+                    className="img-fluid rounded-3"
+                    alt="main banner"
+                  />
+                  <div className="small-banner-content position-absolute">
+                    <h4>SUPERCHARGED FOR PROS.</h4>
+                    <h5>iPad s13+ Pro.</h5>
+                    <p>
+                      From 160000LKR <br /> or 200000LKR
+                    </p>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
-          <div className='col-6'></div>
         </div>
-      </div>
-    </section>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
