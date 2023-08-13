d937382b4c013870cd8e626fa607bb0bbfd9d24f
P A Chathura Madhava <chathuratechdev@gmail.com>
2023-08-13 11:37:52 +0530

    apply section two in Home Page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -86,6 +86,51 @@ const Home = () => {
           </div>
         </div>
       </section>
+      <section className="home-wrapper-2 py-5">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <div className="services d-flex align-items-center justify-content-between">
+                <div>
+                  <img src="images/service.png" alt="services"/>
+                  <div>
+                    <h6></h6>
+                    <p></p>
+                  </div>
+                </div>
+                <div>
+                  <img src="images/service-02.png" alt="services"/>
+                  <div>
+                    <h6></h6>
+                    <p></p>
+                  </div>
+                </div>
+                <div>
+                  <img src="images/service-03.png" alt="services"/>
+                  <div>
+                    <h6></h6>
+                    <p></p>
+                  </div>
+                </div>
+                <div>
+                  <img src="images/service-04.png" alt="services"/>
+                  <div>
+                    <h6></h6>
+                    <p></p>
+                  </div>
+                </div>
+                <div>
+                  <img src="images/service-05.png" alt="services"/>
+                  <div>
+                    <h6></h6>
+                    <p></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
