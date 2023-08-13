8e9410785597d5f02dc15d28812fcbc461396c4e
P A Chathura Madhava <chathuratechdev@gmail.com>
2023-08-13 21:07:46 +0530

    apply featured wrapper

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,18 +1,38 @@
 import React from "react";
+import ReactStars from "react-rating-stars-component";
+import { Link } from "react-router-dom";
 
 const ProductCard = () => {
   return (
     <div className="col-3">
-      <div className="product-card">
+      <div className="product-card position-relative">
+        <div className="wishlist-icon position-absolute">
+          <Link><img src="images/wish.svg" alt="wishlist"/></Link>
+        </div>
         <div className="product-image">
           <img src="images/watch.jpg" alt="product image" />
+          <img src="images/watch-1.jpg" alt="product image" />
         </div>
         <div className="product-details">
           <h6 className="brand"> Havels</h6>
           <h5 className="product-title">
             Kids headphones bulk 10 pack multi colored for students
           </h5>
+          <ReactStars
+            count={5}
+            size={24}
+            value="3"
+            edit={false}
+            activeColor="#ffd700"
+          />
           <p className="price">1000LKR</p>
+        </div>
+        <div className="action-bar position-absolute">
+          <div className="d-flex flex-column gap-15">
+            <Link><img src="images/prodcompare.svg" alt="compare"/></Link>
+            <Link><img src="images/view.svg" alt="view"/></Link>
+            <Link><img src="images/add-cart.svg" alt="add cart"/></Link>
+          </div>
         </div>
       </div>
     </div>
