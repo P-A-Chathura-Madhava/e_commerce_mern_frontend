317a670691572a942a132515c53aa62b1590ac7d
P A Chathura Madhava <chathuratechdev@gmail.com>
2023-08-16 11:31:35 +0530

    apply blogs page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -332,10 +332,21 @@ const Home = () => {
             <div className="col-12">
               <h3 className="section-heading">Our Latest Blogs</h3>
             </div>
+            
+          </div>
+          <div className="row">
+            <div className="col-3">
             <BlogCard />
+            </div>
+            <div className="col-3">
             <BlogCard />
+            </div>
+            <div className="col-3">
             <BlogCard />
+            </div>
+            <div className="col-3">
             <BlogCard />
+            </div>
           </div>
         </div>
       </section>
