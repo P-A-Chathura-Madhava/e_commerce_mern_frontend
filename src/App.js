d0470bcbdb00988a511af7d75c5b7c249b91c95f
P A Chathura Madhava <chathuratechdev@gmail.com>
2023-08-16 14:50:42 +0530

    apply login page

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import OurStore from "./pages/OurStore";
 import Blog from "./pages/Blog";
 import CompareProduct from "./pages/CompareProduct";
 import Wishlist from "./pages/Wishlist";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -23,6 +24,7 @@ function App() {
             <Route path="blogs" element={<Blog/>}/>
             <Route path="compare-product" element={<CompareProduct/>}/>
             <Route path="wishlist" element={<Wishlist/>}/>
+            <Route path="login" element={<Login/>}/>
           </Route>
         </Routes>
       </BrowserRouter>
