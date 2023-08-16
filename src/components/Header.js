195784311341a9717a52197cab2030afd8c6c1e9
P A Chathura Madhava <chathuratechdev@gmail.com>
2023-08-16 13:12:43 +0530

    apply compare product page

--- src/components/Header.js
+++ src/components/Header.js
@@ -49,7 +49,7 @@ const Header = () => {
             <div className="col-5">
               <div className="header-upper-links d-flex align-items-center justify-content-between">
                 <div>
-                  <Link className="d-flex align-items-center gap-10 text-white">
+                  <Link to="/compare-product" className="d-flex align-items-center gap-10 text-white">
                     <img src="images/compare.svg" alt="" />
                     <p className="mb-0">
                       Compare <br /> Products
@@ -57,7 +57,7 @@ const Header = () => {
                   </Link>
                 </div>
                 <div>
-                  <Link className="d-flex align-items-center gap-10 text-white">
+                  <Link to="/wishlist" className="d-flex align-items-center gap-10 text-white">
                     <img src="images/compare.svg" alt="wishlist" />
                     <p className="mb-0">
                       Favourite <br /> Wishlist
@@ -65,7 +65,7 @@ const Header = () => {
                   </Link>
                 </div>
                 <div>
-                  <Link className="d-flex align-items-center gap-10 text-white">
+                  <Link to="/login" className="d-flex align-items-center gap-10 text-white">
                     <img src="images/user.svg" alt="user" />
                     <p className="mb-0">
                       Login <br /> My Account
@@ -73,7 +73,7 @@ const Header = () => {
                   </Link>
                 </div>
                 <div>
-                  <Link className="d-flex align-items-center gap-10 text-white">
+                  <Link to="/cart" className="d-flex align-items-center gap-10 text-white">
                     <img src="images/cart.svg" alt="cart" />
                     <div className="d-flex flex-column gap-10">
                       <span className="badge bg-white text-dark"></span>
