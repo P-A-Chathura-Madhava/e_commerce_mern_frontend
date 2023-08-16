0dbc6da9294f82c3df7ddb6895fc9d04c330cdbc
P A Chathura Madhava <chathuratechdev@gmail.com>
2023-08-16 10:49:19 +0530

    apply our store products

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,41 +1,118 @@
 import React from "react";
 import ReactStars from "react-rating-stars-component";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
-const ProductCard = () => {
+const ProductCard = (props) => {
+  let location = useLocation();
+  const { grid } = props;
   return (
-    <div className="col-3">
-      <Link className="product-card position-relative">
-        <div className="wishlist-icon position-absolute">
-          <Link><img src="images/wish.svg" alt="wishlist"/></Link>
-        </div>
-        <div className="product-image">
-          <img src="images/watch.jpg" className="img-fluid" alt="product image" />
-          <img src="images/watch-1.jpg" className="img-fluid" alt="product image" />
-        </div>
-        <div className="product-details">
-          <h6 className="brand"> Havels</h6>
-          <h5 className="product-title">
-            Kids headphones bulk 10 pack multi colored for students
-          </h5>
-          <ReactStars
-            count={5}
-            size={24}
-            value={4}
-            edit={false}
-            activeColor="#ffd700"
-          />
-          <p className="price">1000LKR</p>
-        </div>
-        <div className="action-bar position-absolute">
-          <div className="d-flex flex-column gap-15">
-            <Link><img src="images/prodcompare.svg" alt="compare"/></Link>
-            <Link><img src="images/view.svg" alt="view"/></Link>
-            <Link><img src="images/add-cart.svg" alt="add cart"/></Link>
+    <>
+      <div
+        className={`${location.pathname == "/store" ? `gr-${grid}` : "col-3"}`}
+      >
+        <Link className="product-card position-relative">
+          <div className="wishlist-icon position-absolute">
+            <Link>
+              <img src="images/wish.svg" alt="wishlist" />
+            </Link>
           </div>
-        </div>
-      </Link>
-    </div>
+          <div className="product-image">
+            <img
+              src="images/watch.jpg"
+              className="img-fluid"
+              alt="product image"
+            />
+            <img
+              src="images/watch-1.jpg"
+              className="img-fluid"
+              alt="product image"
+            />
+          </div>
+          <div className="product-details">
+            <h6 className="brand"> Havels</h6>
+            <h5 className="product-title">
+              Kids headphones bulk 10 pack multi colored for students
+            </h5>
+            <ReactStars
+              count={5}
+              size={24}
+              value={4}
+              edit={false}
+              activeColor="#ffd700"
+            />
+            <p className="price">1000LKR</p>
+          </div>
+          <div className="action-bar position-absolute">
+            <div className="d-flex flex-column gap-15">
+              <Link>
+                <img src="images/prodcompare.svg" alt="compare" />
+              </Link>
+              <Link>
+                <img src="images/view.svg" alt="view" />
+              </Link>
+              <Link>
+                <img src="images/add-cart.svg" alt="add cart" />
+              </Link>
+            </div>
+          </div>
+        </Link>
+      </div>
+      <div
+        className={`${location.pathname == "/store" ? `gr-${grid}` : "col-3"}`}
+      >
+        <Link className="product-card position-relative">
+          <div className="wishlist-icon position-absolute">
+            <Link>
+              <img src="images/wish.svg" alt="wishlist" />
+            </Link>
+          </div>
+          <div className="product-image">
+            <img
+              src="images/watch.jpg"
+              className="img-fluid"
+              alt="product image"
+            />
+            <img
+              src="images/watch-1.jpg"
+              className="img-fluid"
+              alt="product image"
+            />
+          </div>
+          <div className="product-details">
+            <h6 className="brand"> Havels</h6>
+            <h5 className="product-title">
+              Kids headphones bulk 10 pack multi colored for students
+            </h5>
+            <ReactStars
+              count={5}
+              size={24}
+              value={4}
+              edit={false}
+              activeColor="#ffd700"
+            />
+            <p className="description">
+              Redundant alt attribute. Screen-readers already announce `img`
+              tags as an image. You don’t need to use the words `image`, `phot
+              o,` or `picture` (or any specified custom words) in the alt prop
+            </p>
+            <p className="price">1000LKR</p>
+          </div>
+          <div className="action-bar position-absolute">
+            <div className="d-flex flex-column gap-15">
+              <Link>
+                <img src="images/prodcompare.svg" alt="compare" />
+              </Link>
+              <Link>
+                <img src="images/view.svg" alt="view" />
+              </Link>
+              <Link>
+                <img src="images/add-cart.svg" alt="add cart" />
+              </Link>
+            </div>
+          </div>
+        </Link>
+      </div>
+    </>
   );
 };
 
--- src/pages/OurStore.js
+++ src/pages/OurStore.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import BreadCrumb from "../components/BreadCrumb";
 import Meta from "../components/Meta";
 import ReactStars from "react-rating-stars-component";
+import ProductCard from "../components/ProductCard";
 
 const OurStore = () => {
+  const [grid, setGrid] = useState(4);
+  const gridSetter = (i) => {
+    setGrid(i);
+  };
   return (
     <>
       <Meta title={"Our Store"} />
@@ -188,32 +193,80 @@ const OurStore = () => {
               </div>
             </div>
             <div className="col-9">
-              <div className="filter-sort-grid">
+              <div className="filter-sort-grid mb-4">
                 <div className="d-flex justify-content-between align-items-center">
-                <div className="d-flex align-items-center gap-10">
-                  <p className="mb-0 d-block" style={{"width":"100px"}}>Sort By:</p>
-                  <select className="form-control form-select" id="">
-                    <option value="manual">Featured</option>
-                    <option value="best-selling" selected="selected">Best selling</option>
-                    <option value="title-ascending">Alphabetically, A-Z</option>
-                    <option value="title-descending">Alphabetically, Z-A</option>
-                    <option value="price-ascending">Price, low to high</option>
-                    <option value="price-descending">Price, high to low</option>
-                    <option value="created-ascending">Date, old to new</option>
-                    <option value="created-descending">Date, new to old</option>
-                  </select>
+                  <div className="d-flex align-items-center gap-10">
+                    <p className="mb-0 d-block" style={{ width: "100px" }}>
+                      Sort By:
+                    </p>
+                    <select className="form-control form-select" id="">
+                      <option value="manual">Featured</option>
+                      <option value="best-selling" selected="selected">
+                        Best selling
+                      </option>
+                      <option value="title-ascending">
+                        Alphabetically, A-Z
+                      </option>
+                      <option value="title-descending">
+                        Alphabetically, Z-A
+                      </option>
+                      <option value="price-ascending">
+                        Price, low to high
+                      </option>
+                      <option value="price-descending">
+                        Price, high to low
+                      </option>
+                      <option value="created-ascending">
+                        Date, old to new
+                      </option>
+                      <option value="created-descending">
+                        Date, new to old
+                      </option>
+                    </select>
+                  </div>
+                  <div className="d-flex align-items-center gap-10">
+                    <p className="totalproducts mb-0">21 Products</p>
+                    <div className="d-flex gap-10 align-items-center grid">
+                      <img
+                        onClick={() => {
+                          setGrid(3);
+                        }}
+                        src="images/gr4.svg"
+                        className="d-block img-fluid"
+                        alt="grid"
+                      />
+                      <img
+                        onClick={() => {
+                          setGrid(4);
+                        }}
+                        src="images/gr3.svg"
+                        className="d-block img-fluid"
+                        alt="grid"
+                      />
+                      <img
+                        onClick={() => {
+                          setGrid(6);
+                        }}
+                        src="images/gr2.svg"
+                        className="d-block img-fluid"
+                        alt="grid"
+                      />
+
+                      <img
+                        onClick={() => {
+                          setGrid(12);
+                        }}
+                        src="images/gr.svg"
+                        className="d-block img-fluid"
+                        alt="grid"
+                      />
+                    </div>
+                  </div>
                 </div>
-                <div className="d-flex align-items-center gap-10">
-                <p className="totalproducts mb-0">21 Products</p>
-                <div className="d-flex gap-10 align-items-center grid">
-                <img src="images/gr4.svg" className="d-block img-fluid" alt="grid"/>
-                <img src="images/gr3.svg" className="d-block img-fluid" alt="grid"/>
-                  <img src="images/gr2.svg" className="d-block img-fluid" alt="grid"/>
-                  
-                  
-                  <img src="images/gr.svg" className="d-block img-fluid" alt="grid"/>
-                </div>
-                </div>
+              </div>
+              <div className="products-list pb-5">
+                <div className="d-flex gap-10 flex-wrap">
+                  <ProductCard grid={grid} />
                 </div>
               </div>
             </div>
