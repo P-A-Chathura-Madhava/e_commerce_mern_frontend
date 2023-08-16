4b4e90c4dcfd5e2ee84bd4480c1a1f4036ddd857
P A Chathura Madhava <chathuratechdev@gmail.com>
2023-08-16 10:02:22 +0530

    apply our store sort bar

--- src/pages/OurStore.js
+++ src/pages/OurStore.js
@@ -149,18 +149,18 @@ const OurStore = () => {
                       />
                     </div>
                     <div className="w-50">
-                    <h5>
-                      Kids headphones bulk 10 pack multi colored for students
-                    </h5>
-                    <ReactStars
-                      count={5}
-                      size={24}
-                      value={4}
-                      edit={false}
-                      activeColor="#ffd700"
-                    />
-                    <b>2000LKR</b>
-                    </div>                    
+                      <h5>
+                        Kids headphones bulk 10 pack multi colored for students
+                      </h5>
+                      <ReactStars
+                        count={5}
+                        size={24}
+                        value={4}
+                        edit={false}
+                        activeColor="#ffd700"
+                      />
+                      <b>2000LKR</b>
+                    </div>
                   </div>
                   <div className="random-products d-flex">
                     <div className="w-50">
@@ -171,23 +171,52 @@ const OurStore = () => {
                       />
                     </div>
                     <div className="w-50">
-                    <h5>
-                      Kids headphones bulk 10 pack multi colored for students
-                    </h5>
-                    <ReactStars
-                      count={5}
-                      size={24}
-                      value={4}
-                      edit={false}
-                      activeColor="#ffd700"
-                    />
-                    <b>2000LKR</b>
-                    </div>                    
+                      <h5>
+                        Kids headphones bulk 10 pack multi colored for students
+                      </h5>
+                      <ReactStars
+                        count={5}
+                        size={24}
+                        value={4}
+                        edit={false}
+                        activeColor="#ffd700"
+                      />
+                      <b>2000LKR</b>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-            <div className="col-9"></div>
+            <div className="col-9">
+              <div className="filter-sort-grid">
+                <div className="d-flex justify-content-between align-items-center">
+                <div className="d-flex align-items-center gap-10">
+                  <p className="mb-0 d-block" style={{"width":"100px"}}>Sort By:</p>
+                  <select className="form-control form-select" id="">
+                    <option value="manual">Featured</option>
+                    <option value="best-selling" selected="selected">Best selling</option>
+                    <option value="title-ascending">Alphabetically, A-Z</option>
+                    <option value="title-descending">Alphabetically, Z-A</option>
+                    <option value="price-ascending">Price, low to high</option>
+                    <option value="price-descending">Price, high to low</option>
+                    <option value="created-ascending">Date, old to new</option>
+                    <option value="created-descending">Date, new to old</option>
+                  </select>
+                </div>
+                <div className="d-flex align-items-center gap-10">
+                <p className="totalproducts mb-0">21 Products</p>
+                <div className="d-flex gap-10 align-items-center grid">
+                <img src="images/gr4.svg" className="d-block img-fluid" alt="grid"/>
+                <img src="images/gr3.svg" className="d-block img-fluid" alt="grid"/>
+                  <img src="images/gr2.svg" className="d-block img-fluid" alt="grid"/>
+                  
+                  
+                  <img src="images/gr.svg" className="d-block img-fluid" alt="grid"/>
+                </div>
+                </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
