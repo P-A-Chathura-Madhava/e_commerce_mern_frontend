f8833be5386d9092472455dd10502285ca654696
P A Chathura Madhava <chathuratechdev@gmail.com>
2023-08-17 11:50:19 +0530

    finalize single product page

--- src/pages/TermsAndConditions.js
+++ src/pages/TermsAndConditions.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import BreadCrumb from "../components/BreadCrumb";
+import Meta from "../components/Meta";
 
 const TermsAndConditions = () => {
   return (
-    <div>TermsAndConditions</div>
+    <>
+    <Meta title={"Terms And Conditions"} />
+      <BreadCrumb title="Terms And Conditions" />
+      <section className='policy-wrapper py-5 home-wrapper-2'>
+        <div className='container-xxl'>
+            <div className='row'>
+                <div className='col-12'>
+                    <div className='policy'></div>
+                </div>
+            </div>
+        </div>
+      </section>
+      </>
   )
 }
 
