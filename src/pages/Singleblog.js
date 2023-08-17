0fd078fd5a679108d9973933684e2d4ff0055e67
P A Chathura Madhava <chathuratechdev@gmail.com>
2023-08-17 09:16:22 +0530

    finalize single blog page

--- src/pages/Singleblog.js
+++ src/pages/Singleblog.js
@@ -1,21 +1,43 @@
-import React from 'react'
+import React from "react";
 import BreadCrumb from "../components/BreadCrumb";
 import Meta from "../components/Meta";
+import { Link } from "react-router-dom";
+import { HiOutlineArrowLeft } from "react-icons/hi";
 
 const Singleblog = () => {
   return (
     <>
-    <Meta title={"Dynamic Blog Name"} />
+      <Meta title={"Dynamic Blog Name"} />
       <BreadCrumb title="Dynamic Blog Name" />
       <div className="blog-wrapper home-wrapper-2 py-5">
         <div className="container-xxl">
           <div className="row">
-            <div className="col-12"></div>
+            <div className="col-12">
+              <div className="single-blog-card">
+                <Link to="/blogs" className="d-flex align-items-center gap-10">
+                  <HiOutlineArrowLeft className="fs-4" />
+                  Go Back to Blogs
+                </Link>
+                <h3 className="title">
+                  A Beautiful Sunday Morning Renaissance
+                </h3>
+                <img
+                  src="images/blog3.webp"
+                  className="img-fluid w-100 my-4"
+                  alt="blog"
+                />
+                <p>
+                  Module not found: Error: Cannot find file: 'Singleblog.js'
+                  does not match the corresponding name on disk:
+                  '.\src\pages\SingleBlo g.js'
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-      </>
-  )
-}
+    </>
+  );
+};
 
 export default Singleblog;
