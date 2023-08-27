5158787e2dbe2509d443046e86ddf510a1bf4339
P A Chathura Madhava <chathuratechdev@gmail.com>
2023-08-27 10:43:50 +0530

    implement Login Page API integration

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -2,47 +2,86 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { authService } from "./userService";
 import { toast } from "react-toastify";
 
-export const registerUser = createAsyncThunk("auth/register", async (userData, thunkAPI) => {
+export const registerUser = createAsyncThunk(
+  "auth/register",
+  async (userData, thunkAPI) => {
     try {
-        return await authService.register(userData);
+      return await authService.register(userData);
     } catch (error) {
-        return thunkAPI.rejectWithValue(error);
+      return thunkAPI.rejectWithValue(error);
     }
-});
+  }
+);
+
+export const loginUser = createAsyncThunk(
+    "auth/login",
+    async (userData, thunkAPI) => {
+      try {
+        return await authService.login(userData);
+      } catch (error) {
+        return thunkAPI.rejectWithValue(error);
+      }
+    }
+  );
 
 const initialState = {
-    user: "",
-    isError: "",
-    isSuccess: "",
-    isLoading: "",
-    message: ""
-}
+  user: "",
+  isError: "",
+  isSuccess: "",
+  isLoading: "",
+  message: "",
+};
 
 export const authSlice = createSlice({
-    name: "auth",
-    initialState: initialState,
-    reducers: {},
-    extraReducers: (builder) => {
-        builder.addCase(registerUser.pending, (state) => {
-            state.isLoading = true;
-        }).addCase(registerUser.fulfilled, (state, action) => {
-            state.isLoading = false;
-            state.isError = false;
-            state.isSuccess = true;
-            state.createdUser=action.payload;
-            if (state.isSuccess === true){
-                toast.info('User created successfully');
-            }
-        }).addCase(registerUser.rejected, (state, action) => {
-            state.isLoading = false;
-            state.isError = true;
-            state.isSuccess = false;
-            state.message = action.error;
-            if (state.isError === true) {
-                toast.error(action.error);
-            }
-        })
-    }
-})
+  name: "auth",
+  initialState: initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(registerUser.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(registerUser.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.isSuccess = true;
+        state.createdUser = action.payload;
+        if (state.isSuccess === true) {
+        alert(JSON.stringify("user logged in"))
+        }
+      })
+      .addCase(registerUser.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.isSuccess = false;
+        state.message = action.error;
+        if (state.isError === true) {
+          toast.error(action.error);
+        }
+      })
+      .addCase(loginUser.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(loginUser.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.isSuccess = true;
+        state.user = action.payload;
+        if (state.isSuccess === true) {
+            localStorage.setItem('token', action.payload.token);
+            toast.info("User logged in successfully");
+        }
+      })
+      .addCase(loginUser.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.isSuccess = false;
+        state.message = action.error;
+        if (state.isError === true) {
+          toast.error(action.error);
+        }
+      });
+  },
+});
 
 export default authSlice.reducer;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -7,106 +7,73 @@ import CustomInput from "../components/CustomInput";
 import { useFormik } from "formik";
 import * as yup from "yup";
 import { useDispatch } from "react-redux";
-import { registerUser } from "../features/user/userSlice";
+import { loginUser } from "../features/user/userSlice";
 
-const signSchema = yup.object({
-  firstname: yup.string().required("First name is required"),
-  lastname: yup.string().required("Last name is required"),
+const loginSchema = yup.object({
   email: yup
     .string()
     .email("Email should be valid")
     .required("Email address is required"),
-  mobile: yup.string().required("Mobile number is required"),
   password: yup.string().required("Password is required"),
 });
 
-const Signup = () => {
+const Login = () => {
   const dispatch = useDispatch();
   const formik = useFormik({
     initialValues: {
-      firstname: "",
-      lastname: "",
       email: "",
-      mobile: "",
       password: "",
     },
-    validationSchema: signSchema,
+    validationSchema: loginSchema,
     onSubmit: (values) => {
-      dispatch(registerUser(values));
+      dispatch(loginUser(values));
     },
   });
   return (
     <>
-      <Meta title={"Sign Up"} />
-      <BreadCrumb title="Sign up" />
+      <Meta title={"Login"} />
+      <BreadCrumb title="Login" />
       <Container class1="login-wrapper py-5 home-wrapper-2">
         <div className="row">
           <div className="col-12">
             <div className="auth-card">
-              <h3 className="text-center mb-3">Sign Up</h3>
+              <h3 className="text-center mb-3">Login</h3>
               <form
                 action=""
                 onSubmit={formik.handleSubmit}
                 className="d-flex flex-column gap-15"
               >
                 <CustomInput
-                  type="text"
-                  name="firstname"
-                  placeholder="first name"
-                  value={formik.values.firstname}
-                  onChange={formik.handleChange("firstname")}
-                  onBlur={formik.handleBlur("firstname")}
-                />
-                <div className="error">
-                  {formik.touched.firstname && formik.errors.firstname}
-                </div>
-                <CustomInput
-                  type="text"
-                  name="lastname"
-                  placeholder="last name"
-                  value={formik.values.lastname}
-                  onChange={formik.handleChange("lastname")}
-                  onBlur={formik.handleBlur("lastname")}
-                />
-                <div className="error">
-                  {formik.touched.lastname && formik.errors.lastname}
-                </div>
-                <CustomInput
                   type="email"
                   name="email"
                   placeholder="Email"
-                  value={formik.values.email}
                   onChange={formik.handleChange("email")}
                   onBlur={formik.handleBlur("email")}
+                  value={formik.values.email}
                 />
                 <div className="error">
                   {formik.touched.email && formik.errors.email}
                 </div>
                 <CustomInput
-                  type="tel"
-                  name="mobile"
-                  placeholder="Mobile Number"
-                  value={formik.values.mobile}
-                  onChange={formik.handleChange("mobile")}
-                  onBlur={formik.handleBlur("mobile")}
-                />
-                <div className="error">
-                  {formik.touched.mobile && formik.errors.mobile}
-                </div>
-                <CustomInput
                   type="password"
                   name="password"
                   placeholder="Password"
-                  value={formik.values.password}
                   onChange={formik.handleChange("password")}
                   onBlur={formik.handleBlur("password")}
+                  value={formik.values.password}
                 />
                 <div className="error">
                   {formik.touched.password && formik.errors.password}
                 </div>
                 <div>
+                  <Link to="/forgot-password">Forgot Password</Link>
                   <div className="mt-3 d-flex justify-content-center gap-15 align-items-center">
-                    <button className="button border-0">Sign Up</button>
+                    <button className="button border-0" type="submit">
+                      Login
+                    </button>
+                    <Link to="/signup" className="button signup">
+                      SignUp
+                    </Link>
                   </div>
                 </div>
               </form>
@@ -118,4 +85,4 @@ const Signup = () => {
   );
 };
 
-export default Signup;
+export default Login;
